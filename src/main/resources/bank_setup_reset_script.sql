72980221ff6a4f70b3ab1c1ce8940fdd5983ba90
justin951 <justin951@revature.com>
2024-07-11 10:06:00 -0400

    change to bank_setup_reset_script

--- src/main/resources/bank_setup_reset_script.sql
+++ src/main/resources/bank_setup_reset_script.sql
@@ -1,8 +1,19 @@
 DROP TABLE IF EXISTS "user";
 
 CREATE TABLE "user"(
+	user_id integer PRIMARY KEY AUTOINCREMENT,
 	username text,
 	password text
 );
 
-INSERT INTO "user" values ('adimn1', 1234);
+INSERT INTO "user" values (0, 'adimn1', 1234);
+
+DROP TABLE IF EXISTS "account";
+
+CREATE TABLE "account"(
+	account_id integer PRIMARY KEY AUTOINCREMENT,
+	balance double NOT NULL DEFAULT 0,
+	primary_user INTEGER NOT null,
+	joint_user INTEGER DEFAULT null
+);
+
